7b9c67d508b3e9b06f2bc7a43e2177d873eefcb3
Jakob Schrettenbrunner <dev@schrej.net>
2017-08-31 23:57:33 +0200

    api: improve routes

--- api/routes.go
+++ api/routes.go
@@ -1,17 +1,9 @@
 package api
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
 func (api *API) registerRoutes() {
 	api.router.GET("/", AuthHandler(""), handleGetIndex)
 	api.router.PATCH("/config", AuthHandler("c:config"), handlePatchConfig)
 
 	api.registerServerRoutes()
 	api.registerServerFileRoutes()
-}
-
-func handle(c *gin.Context) {
-
 }
--- api/routes_server_files.go
+++ api/routes_server_files.go
@@ -1,20 +1,20 @@
 package api
 
 func (api *API) registerServerFileRoutes() {
-	api.router.GET("/server/:server/files/file/:file", AuthHandler("s:files:read"), handleGetFile)
-	api.router.GET("/server/:server/files/stat/:file", AuthHandler("s:files:"), handleGetFileStat)
-	api.router.GET("/server/:server/files/dir/:directory", AuthHandler("s:files:get"), handleGetDirectory)
+	api.router.GET("/servers/:server/files/file/:file", AuthHandler("s:files:read"), handleGetFile)
+	api.router.GET("/servers/:server/files/stat/:file", AuthHandler("s:files:"), handleGetFileStat)
+	api.router.GET("/servers/:server/files/dir/:directory", AuthHandler("s:files:get"), handleGetDirectory)
 
-	api.router.POST("/server/:server/files/dir/:directory", AuthHandler("s:files:create"), handlePostFilesFolder)
-	api.router.POST("/server/:server/files/file/:file", AuthHandler("s:files:post"), handlePostFile)
+	api.router.POST("/servers/:server/files/dir/:directory", AuthHandler("s:files:create"), handlePostFilesFolder)
+	api.router.POST("/servers/:server/files/file/:file", AuthHandler("s:files:post"), handlePostFile)
 
-	api.router.POST("/server/:server/files/copy/:file", AuthHandler("s:files:copy"), handlePostFileCopy)
-	api.router.POST("/server/:server/files/move/:file", AuthHandler("s:files:move"), handlePostFileMove)
-	api.router.POST("/server/:server/files/rename/:file", AuthHandler("s:files:move"), handlePostFileMove)
-	api.router.POST("/server/:server/files/compress/:file", AuthHandler("s:files:compress"), handlePostFileCompress)
-	api.router.POST("/server/:server/files/decompress/:file", AuthHandler("s:files:decompress"), handlePostFileDecompress)
+	api.router.POST("/servers/:server/files/copy/:file", AuthHandler("s:files:copy"), handlePostFileCopy)
+	api.router.POST("/servers/:server/files/move/:file", AuthHandler("s:files:move"), handlePostFileMove)
+	api.router.POST("/servers/:server/files/rename/:file", AuthHandler("s:files:move"), handlePostFileMove)
+	api.router.POST("/servers/:server/files/compress/:file", AuthHandler("s:files:compress"), handlePostFileCompress)
+	api.router.POST("/servers/:server/files/decompress/:file", AuthHandler("s:files:decompress"), handlePostFileDecompress)
 
-	api.router.DELETE("/server/:server/files/file/:file", AuthHandler("s:files:delete"), handleDeleteFile)
+	api.router.DELETE("/servers/:server/files/file/:file", AuthHandler("s:files:delete"), handleDeleteFile)
 
-	api.router.GET("/server/:server/files/download/:token", handleGetDownloadFile)
+	api.router.GET("/servers/:server/files/download/:token", handleGetDownloadFile)
 }
