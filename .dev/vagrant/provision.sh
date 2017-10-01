d1fdc713f5375621980851ca6b6f93a7f24b59ac
Jakob Schrettenbrunner <dev@schrej.net>
2017-10-01 20:39:59 +0200

    improve vagrant provision script

--- .dev/vagrant/provision.sh
+++ .dev/vagrant/provision.sh
@@ -4,6 +4,7 @@ echo "Provisioning development environment for Pterodactyl go daemon."
 cp /home/ubuntu/go/github.com/Pterodactyl/wings.go/.dev/vagrant/motd.txt /etc/motd
 
 chown -R ubuntu:ubuntu /home/ubuntu/go
+chown -R ubuntu:ubuntu /srv
 
 echo "Update apt repositories"
 sudo add-apt-repository ppa:longsleep/golang-backports
@@ -12,6 +13,7 @@ apt-get update > /dev/null
 echo "Install docker"
 curl -sSL https://get.docker.com/ | sh
 systemctl enable docker
+usermod -aG docker ubuntu
 
 echo "Install go"
 apt-get install -y golang-go
@@ -21,6 +23,9 @@ echo 'export PATH=$PATH:$GOPATH/bin' >> /home/ubuntu/.profile
 
 echo "Install go dep"
 sudo -H -u ubuntu bash -c 'go get -u github.com/golang/dep/cmd/dep'
+
+echo "Install delve for debugging"
+sudo -H -u ubuntu bash -c 'go get -u github.com/derekparker/delve/cmd/dlv'
 
 echo "Install additional dependencies"
 apt-get -y install mercurial #tar unzip make gcc g++ python > /dev/null
