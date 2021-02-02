e3b0b9191281096bb052c0aa521caec172b4e80b
Dane Everitt <dane@daneeveritt.com>
2021-02-01 21:33:03 -0800

    Don't tempt people with this public interface

--- server/manager.go
+++ server/manager.go
@@ -46,12 +46,6 @@ func NewEmptyManager(client remote.Client) *Manager {
 	return &Manager{client: client}
 }
 
-// Client returns the HTTP client interface that allows interaction with the
-// Panel API.
-func (m *Manager) Client() remote.Client {
-	return m.client
-}
-
 // Put replaces all of the current values in the collection with the value that
 // is passed through.
 func (m *Manager) Put(s []*Server) {
