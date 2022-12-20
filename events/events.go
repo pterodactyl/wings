92662a93b294b69863a0dcffc4f212742df55c28
Nice-Hosting ApS <37418674+Nice-Hosting@users.noreply.github.com>
2022-12-20 20:35:17 +0100

    Update events.go
    Remove topic splitting

--- events/events.go
+++ events/events.go
@@ -1,8 +1,6 @@
 package events
 
 import (
-	"strings"
-
 	"emperror.dev/errors"
 	"github.com/goccy/go-json"
 
@@ -34,18 +32,6 @@ func NewBus() *Bus {
 
 // Publish publishes a message to the Bus.
 func (b *Bus) Publish(topic string, data interface{}) {
-	// Some of our actions for the socket support passing a more specific namespace,
-	// such as "backup completed:1234" to indicate which specific backup was completed.
-	//
-	// In these cases, we still need to send the event using the standard listener
-	// name of "backup completed".
-	if strings.Contains(topic, ":") {
-		parts := strings.SplitN(topic, ":", 2)
-		if len(parts) == 2 {
-			topic = parts[0]
-		}
-	}
-
 	enc, err := json.Marshal(Event{Topic: topic, Data: data})
 	if err != nil {
 		panic(errors.WithStack(err))
