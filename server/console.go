93a68236409d4650368f60855b17cca627729a0f
guoguangwu <guoguangwu@magic-shield.com>
2023-09-01 09:52:02 +0800

    chore: unnecessary use of fmt.Sprintf

--- server/console.go
+++ server/console.go
@@ -37,7 +37,7 @@ func (s *Server) Throttler() *ConsoleThrottle {
 
 		s.throttler = newConsoleThrottle(throttles.Lines, period)
 		s.throttler.strike = func() {
-			s.PublishConsoleOutputFromDaemon(fmt.Sprintf("Server is outputting console data too quickly -- throttling..."))
+			s.PublishConsoleOutputFromDaemon("Server is outputting console data too quickly -- throttling...")
 		}
 	})
 	return s.throttler
