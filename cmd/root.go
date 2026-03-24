4b2191cf3702b0b2ae3902c795a5aa12ef13ed4c
Bas <ImBas@users.noreply.github.com>
2026-03-24 10:57:13 +0100

    Fix version command output format
    %v already outputs like 'v1.12.1' so using v%v outputs 'vv1.12.1'

--- cmd/root.go
+++ cmd/root.go
@@ -63,7 +63,7 @@ var versionCommand = &cobra.Command{
 	Use:   "version",
 	Short: "Prints the current executable version and exits.",
 	Run: func(cmd *cobra.Command, _ []string) {
-		fmt.Printf("wings v%s\nCopyright © 2018 - %d Dane Everitt & Contributors\n", system.Version, time.Now().Year())
+		fmt.Printf("wings %s\nCopyright © 2018 - %d Dane Everitt & Contributors\n", system.Version, time.Now().Year())
 	},
 }
 
