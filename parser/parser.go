1863e4d6edb205317864a7e2e64fbcfb51731dac
jessefaler <protoxon.owner@gmail.com>
2026-03-27 21:18:50 -0500

    fix(parser): return error for unknown parser type
    Previously, if the parser type did not match any valid types, Parse
    would silently do nothing and return nil as if it succeeded. This
    made it easy to miss typos or invalid parser types.
    
    Now, an unknown parser type will return an error, making it easier
    to catch mistakes.

--- parser/parser.go
+++ parser/parser.go
@@ -215,7 +215,10 @@ func (f *ConfigurationFile) Parse(file ufs.File) error {
 		err = f.parseIniFile(file)
 	case Xml:
 		err = f.parseXmlFile(file)
+	default:
+		return errors.Errorf("parser: unknown parser type %q", f.Parser)
 	}
+
 	return err
 }
 
